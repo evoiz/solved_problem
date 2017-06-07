ac67a7e7da62faef1519a0692c7d65c368c2a5fa
evoiz <evoiz963@gmail.com>
2017-06-07 14:00:58 +0200

    Create 483A-Counterexample.cpp

---
+++ 483A-Counterexample.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a,b;
+int main(){
+	cin >> a >> b;
+	if ((b-a + 1) < 3) {cout << -1 << endl;return 0;}
+	if (a%2==0){cout << a << " " << a + 1 << " " << a + 2 << endl;return 0;}
+    if ((b-a + 1) > 3){cout << a + 1 << " " << a + 2 << " " << a + 3 << endl;return 0;}
+	cout << -1 << endl;
+}
