bc33f7954f00b4bf3fb7ec629d1fa12b776495cc
evoiz <evoiz963@gmail.com>
2017-06-08 11:38:01 +0200

    Create 570B-Simple_Game.cpp

---
+++ 570B-Simple_Game.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long  a,b;
+int main() {
+    cin>>a>>b;
+    if(a==1){cout<<1<<endl;return 0;}
+    if(b>(a/2)){cout<<b-1<<endl;return 0;}
+    cout<<b+1<<endl;
+    return 0;
+}
